43d9e2c0d95f9aafda4134367d60101c5c0bf56b
Linus Schaub <linus@5dlab.com>
2016-10-01 13:33:11 +0200

    Added VR scripts

---
+++ Assets/ImportedAssets/VRStandardAssets/Scripts/VRClickUIHelper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+
+public class VRClickUIHelper : MonoBehaviour {
+
+	public UnityEvent onClickEvents;
+
+	public void InvokeEvents() {
+		 onClickEvents.Invoke();
+	}
+
+	void OnEnable() {
+		VRInteractiveItem item = gameObject.GetComponent<VRInteractiveItem>();
+		item.OnClick += InvokeEvents;
+	}
+
+	void OnDisable() {
+		VRInteractiveItem item = gameObject.GetComponent<VRInteractiveItem>();
+		item.OnClick -= InvokeEvents;
+	}
+}
