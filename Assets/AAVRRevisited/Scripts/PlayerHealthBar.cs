13b86bb614b256a631e363fa877799e525532fe0
Linus Schaub <linus@5dlab.com>
2016-10-06 20:46:08 +0200

    Added Health Bar text

--- Assets/AAVRRevisited/Scripts/PlayerHealthBar.cs
+++ Assets/AAVRRevisited/Scripts/PlayerHealthBar.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerHealthBar : MonoBehaviour, IPlayerEventTarget {
 
 	public RectTransform maskRectTransform;
+	public Text healthBarText;
 	private Player player;
 	private float defaultWidth;
 	// Use this for initialization
@@ -19,6 +21,9 @@ public class PlayerHealthBar : MonoBehaviour, IPlayerEventTarget {
 			RectTransform newMaskRect = maskRectTransform;
 			float newWidth = this.defaultWidth * ((float) player.getHealth()/(float) player.getMaxHealth());
 			this.maskRectTransform.sizeDelta = new Vector2( newWidth, newMaskRect.sizeDelta.y);
+		}
+		if(healthBarText!= null) {
+			healthBarText.text = player.getHealth()+"/"+player.getMaxHealth();
 		}
 	}
 }
