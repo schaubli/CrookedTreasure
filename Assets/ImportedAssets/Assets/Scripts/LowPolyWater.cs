2345f1a30c846ef89f941fb21bda4ad54b3b777a
Linus Schaub <Linus Schaub>
2016-10-09 14:19:20 +0200

    Added Water in VR World

---
+++ Assets/ImportedAssets/Assets/Scripts/LowPolyWater.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(Renderer))]
+public class LowPolyWater : MonoBehaviour
+{
+    [SerializeField]
+    private SineWaves _sineWaves = null;
+    [SerializeField, Tooltip("Useful for testing sine wave setups")]
+    private bool _updateMaterialPerFrame = false;
+    [Header("Quad Detail"), SerializeField, InspectorButton("SubdivideMesh")]
+    private bool _subdivideMesh;
+
+    private Material _lowPolyWater = null;
+
+    /// <summary>
+    /// Subdivides each quad of the mesh for smaller quads
+    /// </summary>
+    private void SubdivideMesh()
+    {
+        Mesh current = GetComponent<MeshFilter>().sharedMesh;
+        Mesh newMesh = Instantiate(current);
+        newMesh.name = current.name;
+        MeshHelper.Subdivide4(newMesh);
+        GetComponent<MeshFilter>().sharedMesh = newMesh;
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        // no waves info
+        if (_sineWaves == null)
+        {
+            enabled = false;
+            return;
+        }
+
+        _lowPolyWater = GetComponent<Renderer>().sharedMaterial;
+
+        if (!_lowPolyWater)
+        {
+            enabled = false;
+            return;
+        }
+			
+        // set general uniforms
+        _lowPolyWater.SetInt("_Waves", _sineWaves.Length);
+        _lowPolyWater.SetFloat("_TimeScale", _sineWaves.Timescale);
+        // set simulation waves parameters
+        for (int i = 0; i < _sineWaves.Length; i++)
+        {
+            var a = _sineWaves[i].amplitude;
+            var f = 2.0f * Mathf.PI / _sineWaves[i].waveLength;
+            var p = _sineWaves[i].speed * f;
+            float radA = _sineWaves[i].travelAngle * Mathf.Deg2Rad;
+            var d = new Vector2(Mathf.Sin(radA), Mathf.Cos(radA));
+            var s = _sineWaves[i].sharpness;
+            _lowPolyWater.SetVector("_SineWave0" + i, new Vector4(a, f, p, 0));
+            _lowPolyWater.SetVector("_SineWave1" + i, new Vector4(d.x, d.y, s, 0));
+        }
+    }
+
+    private void Update()
+    {
+        if (_updateMaterialPerFrame)
+        {
+            // set general uniforms
+            _lowPolyWater.SetInt("_Waves", _sineWaves.Length);
+            _lowPolyWater.SetFloat("_TimeScale", _sineWaves.Timescale);
+            // set simulation waves parameters
+            for (int i = 0; i < _sineWaves.Length; i++)
+            {
+                var a = _sineWaves[i].amplitude;
+                var f = 2.0f * Mathf.PI / _sineWaves[i].waveLength;
+                var p = _sineWaves[i].speed * f;
+                float radA = _sineWaves[i].travelAngle * Mathf.Deg2Rad;
+                var d = new Vector2(Mathf.Sin(radA), Mathf.Cos(radA));
+                var s = _sineWaves[i].sharpness;
+                _lowPolyWater.SetVector("_SineWave0" + i, new Vector4(a, f, p, 0));
+                _lowPolyWater.SetVector("_SineWave1" + i, new Vector4(d.x, d.y, s, 0));
+            }
+        }
+    }
+}
---
+++ Assets/hideflags.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hideflags : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Mesh mat = gameObject.GetComponent<MeshFilter>().mesh;
+		mat.hideFlags = HideFlags.None;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
