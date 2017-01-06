397a0322963fd416a99b202597ff6b1cb1c91700
Linus Schaub <linus@5dlab.com>
2017-01-06 17:46:29 +0100

    Added Treasure digging scene and VR Island

---
+++ Assets/AAVRRevisited/Scripts/Diggable.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+public class Diggable : MonoBehaviour {
+
+	public int diggCount = 0; //How often this has been digged
+	public bool isFinished = false;
+	public Vector3 startPosition;
+
+	public void Digg() {
+		ShovelController.Instance.OnClick();
+		if(this.isFinished == false && this.diggCount < DiggController.Instance.maxDiggCount) {
+			this.diggCount++;
+			Vector3 surfacePosition = this.transform.position;
+			surfacePosition.y -= DiggController.Instance.diggOffset;
+			this.transform.position = surfacePosition;
+
+			if(this.diggCount == DiggController.Instance.maxDiggCount) {
+				this.isFinished = true;
+			}
+		}
+	}
+
+	public void OnOver() { // When Player hovers over Collider
+		ShovelController.Instance.OnHoverOverDiggable(this);
+	}
+
+	public void OnOut() { // When Player stops hovering over Collider
+		ShovelController.Instance.OnEndHoverOverDiggable(this);
+	}
+	void OnEnable() {
+		this.startPosition = this.transform.localPosition;
+		VRInteractiveItem item = gameObject.GetComponent<VRInteractiveItem>();
+		item.OnOver += OnOver;
+		item.OnClick += Digg;
+		item.OnOut += OnOut;
+	}
+
+	void OnDisable() {
+		VRInteractiveItem item = gameObject.GetComponent<VRInteractiveItem>();
+		item.OnOver -= OnOver;
+		item.OnClick -= Digg;
+		item.OnOut -= OnOut;
+	}
+
+	public void Reset() {
+		if(this.startPosition == Vector3.zero) {
+			OnEnable();
+		}
+		this.isFinished = false;
+		this.diggCount = 0;
+		this.transform.localPosition = startPosition;
+	}
+}
---
+++ Assets/AAVRRevisited/Scripts/TreasureController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+public class TreasureController : MonoBehaviour {
+
+	// Use this for initialization
+	public void Open() {
+		GetComponentInChildren<Animator>().Play("TreasureChestOpen");
+		Invoke("ActivateParticles", 3);
+	}
+
+	private void ActivateParticles() {
+		GetComponentInChildren<ParticleSystem>().Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
