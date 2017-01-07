b4d06f166683eebf9c825698c0b287583bf8fc62
Linus Schaub <linus@5dlab.com>
2017-01-07 12:11:03 +0100

    Added missing Inspector button scripts

---
+++ Assets/Editor/BehaviourButtonsEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+/// <summary>
+/// Custom editor for all MonoBehaviour scripts in order to draw buttons for all button attributes (<see cref="ButtonAttribute"/>).
+/// </summary>
+[CustomEditor( typeof( MonoBehaviour ), true, isFallback = true )]
+[CanEditMultipleObjects]
+public class BehaviourButtonsEditor : Editor
+{
+	private ButtonAttributeHelper helper = new ButtonAttributeHelper();
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		helper.DrawButtons();
+	}
+
+	private void OnEnable()
+	{
+		helper.Init( target );
+	}
+}
---
+++ Assets/Scripts/ButtonAttributeHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+
+/// <summary>
+/// Searches through a target class in order to find all button attributes (<see cref="ButtonAttribute"/>).
+/// </summary>
+public class ButtonAttributeHelper
+{
+    private static object[] emptyParamList = new object[0];
+
+    private IList<MethodInfo> methods = new List<MethodInfo>();
+    private Object targetObject;
+
+    public void Init(Object targetObject)
+    {
+        this.targetObject = targetObject;
+        methods =
+            targetObject.GetType()
+                .GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(m =>
+                        m.GetCustomAttributes(typeof(ButtonAttribute), false).Length == 1 &&
+                        m.GetParameters().Length == 0 &&
+                        !m.ContainsGenericParameters
+                ).ToList();
+    }
+
+    public void DrawButtons()
+    {
+        if (methods.Count > 0)
+        {
+            //EditorGUILayout.HelpBox("Click to execute methods!", MessageType.None);
+            ShowMethodButtons();
+        }
+    }
+
+    private void ShowMethodButtons()
+    {
+        foreach (MethodInfo method in methods)
+        {
+            #if UNITY_EDITOR
+            string buttonText = ObjectNames.NicifyVariableName(method.Name);
+            if (GUILayout.Button(buttonText))
+            {
+                method.Invoke(targetObject, emptyParamList);
+            }
+            #endif
+        }
+    }
+}
